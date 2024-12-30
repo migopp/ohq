8658480a0b52addab84f1424575ccfdcae4a9610
Michael Goppert <goppert@utexas.edu>
2024-12-29 23:26:58 -0600

    Add admin field in schema

--- internal/db/models.go
+++ internal/db/models.go
@@ -6,4 +6,5 @@ type User struct {
 	gorm.Model
 	Username string `gorm:"unique;not null" yaml:"username"`
 	Password string `gorm:"not null" yaml:"password"`
+	Admin    bool   `gorm:"default:false" yaml:"admin"`
 }
